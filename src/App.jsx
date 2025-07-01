c5ddd69405de4caa625a931554f0353db359b479
fnuni <francesco.nucci@unisalento.it>
2025-07-01 17:47:44 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-gray-800">Benvenuto nella piattaforma 3DCP</h1>
+      <p className="mt-4 text-lg text-gray-600">Esplora, configura, analizza ed esporta componenti edilizi stampabili in 3D</p>
+    </div>
+  );
+}
